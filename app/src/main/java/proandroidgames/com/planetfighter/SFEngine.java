b35a83fc49d7a4e46622238cceb517a1e5cd86ec
nilzxq <2013358717@qq.com>
2017-03-24 20:53:56 +0800

    connection button

--- app/src/main/java/proandroidgames/com/planetfighter/SFEngine.java
+++ app/src/main/java/proandroidgames/com/planetfighter/SFEngine.java
@@ -1,5 +1,6 @@
 package proandroidgames.com.planetfighter;
 
+import android.view.View;
 /**
  * Created by zxq on 2017/3/24 0024.
  */
@@ -7,4 +8,15 @@ package proandroidgames.com.planetfighter;
 public class SFEngine {
     /*Constans that will be used in the game*/
     public static final int GAME_THREAD_DELAY=4000;
+    public static final int MENU_BUTTON_ALPHA=0;
+    public static final boolean HAPTIC_BUTTON_FEEDBACK=true;
+    /*Kill game and exit*/
+    public boolean onExit(View v){
+        try {
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+
+    }
 }
--- app/src/main/java/proandroidgames/com/planetfighter/Sfmainmenu.java
+++ app/src/main/java/proandroidgames/com/planetfighter/Sfmainmenu.java
@@ -3,6 +3,7 @@ package proandroidgames.com.planetfighter;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 
 /**
@@ -14,6 +15,13 @@ public class Sfmainmenu extends Activity{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starfighter);
+        /*set menu button options*/
+        ImageButton start=(ImageButton)findViewById(R.id.btnStart);
+        ImageButton exit=(ImageButton)findViewById(R.id.btnExit);
+        start.getBackground().setAlpha(SFEngine.MENU_BUTTON_ALPHA);
+        start.setHapticFeedbackEnabled(SFEngine.HAPTIC_BUTTON_FEEDBACK);
+        exit.getBackground().setAlpha(SFEngine.MENU_BUTTON_ALPHA);
+        exit.setHapticFeedbackEnabled(SFEngine.HAPTIC_BUTTON_FEEDBACK);
     }
 
 }
