b79684aa9c121c873d5a80dde5d0059ffd273b35
nilzxq <2013358717@qq.com>
2017-04-03 08:53:33 +0800

    test

--- app/src/main/java/proandroidgames/com/planetfighter/Sfmainmenu.java
+++ app/src/main/java/proandroidgames/com/planetfighter/Sfmainmenu.java
@@ -1,8 +1,8 @@
 package proandroidgames.com.planetfighter;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 
@@ -15,6 +15,8 @@ public class Sfmainmenu extends Activity{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starfighter);
+        final SFEngine engine=new SFEngine();
+
         /*set menu button options*/
         ImageButton start=(ImageButton)findViewById(R.id.btnStart);
         ImageButton exit=(ImageButton)findViewById(R.id.btnExit);
@@ -22,6 +24,25 @@ public class Sfmainmenu extends Activity{
         start.setHapticFeedbackEnabled(SFEngine.HAPTIC_BUTTON_FEEDBACK);
         exit.getBackground().setAlpha(SFEngine.MENU_BUTTON_ALPHA);
         exit.setHapticFeedbackEnabled(SFEngine.HAPTIC_BUTTON_FEEDBACK);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*start game!!!*/
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean clean=false;
+                clean=engine.onExit(v);
+                if(clean)
+                {
+                    int pid=android.os.Process.myPid();
+                    android.os.Process.killProcess(pid);
+                }
+            }
+        });
     }
 
 }
